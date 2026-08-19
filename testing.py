c70267ca60061bf616c2575f622f79cfc0742a14
prathapsunka <102214155+prathapsunka@users.noreply.github.com>
2026-08-19 23:19:45 +0530

    added patient 360 communication Updated with Design Prathap reddy sunka

--- testing.py
+++ testing.py
@@ -1,3 +1,4 @@
 print('welcome to campaign studio on 2026-08-20');
 print('welcome to Patient 360 COmmunication on 2026-08-20');
 print('welcome to Patient 360 Application on 2026-08-20');
+print('Design welcome to Patient 360 Application on 2026-08-20');
